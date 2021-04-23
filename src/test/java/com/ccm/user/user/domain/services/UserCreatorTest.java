953dff94a785faffba3847d834f4fe784fed12be
Miquel Jordà Lopez <miquelin9@gmail.com>
2021-04-23 18:09:25 +0200

    WIP Tests

--- src/test/java/com/ccm/user/user/domain/services/UserCreatorTest.java
+++ src/test/java/com/ccm/user/user/domain/services/UserCreatorTest.java
@@ -1,37 +1,57 @@
 package com.ccm.user.user.domain.services;
 
 import com.ccm.user.user.domain.aggregate.User;
-import com.ccm.user.user.domain.exceptions.FavouritePokemonAlreadyExistsException;
 import com.ccm.user.user.domain.exceptions.UserAlreadyExistsException;
 import com.ccm.user.user.domain.exceptions.UserNotFoundException;
 import com.ccm.user.user.domain.interfaces.UserRepository;
-import com.ccm.user.user.domain.vo.FavouritePokemonId;
 import com.ccm.user.user.domain.vo.UserId;
 import com.ccm.user.user.domain.vo.UserName;
 import com.ccm.user.user.infrastructure.InMemoryUserRepository;
-import io.quarkus.test.Mock;
 import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.inject.Inject;
 
+import java.rmi.UnexpectedException;
+
+import static org.mockito.Mockito.mock;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @QuarkusTest
 public class UserCreatorTest {
     @Inject
-    UserCreator userCreator;
+    UserCreator tested;
 
     @Test
-    public void shouldCreateUser() throws UserAlreadyExistsException {
-        UserId userId = new UserId(1);
-        UserName userName = new UserName("keko");
-        User user = new User(userName, userId);
+    public void verify_createUser_callsToMethods() throws UserAlreadyExistsException, UserNotFoundException {
+        User user = mock(User.class);
 
-        UserRepository userRepository = Mockito.mock(InMemoryUserRepository.class);
+        UserRepository userRepository = mock(InMemoryUserRepository.class);
         Mockito.doNothing().when(userRepository).create(user);
         QuarkusMock.installMockForType(userRepository, UserRepository.class);
 
-        userCreator.createUser(user);
+        tested.createUser(user);
+        Mockito.verify(userRepository, Mockito.times(1)).create(Mockito.any());
+    }
+
+    @Test()
+    public void verify_createUser_throwsUserAlreadyExistsException_whenUserIsAlreadyCreated() throws UserAlreadyExistsException, UserNotFoundException {
+        User user = mock(User.class);
+
+        UserRepository userRepository = mock(InMemoryUserRepository.class);
+        Mockito.doThrow(new UserAlreadyExistsException("BlaBla")).when(userRepository).create(user);
+        QuarkusMock.installMockForType(userRepository, UserRepository.class);
+
+        tested.createUser(user);
+        Mockito.verify(userRepository, Mockito.times(1)).create(Mockito.any());
     }
 }
